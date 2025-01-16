301d904caecad97629d0a74f7f9d451f228a9037
r4ff1n <r4ff1n@proton.me>
2025-01-16 20:57:57 +0900

    2025.01.16

---
+++ Python/001.py
@@ -0,0 +1,48 @@
+#python -V
+#tar -xvf Python-3.12.3.tgz
+#cd Python-3.12.3
+#./configure
+#make
+#su -
+#make install
+
+import sys
+sys.exit()
+#interpreter
+#Ctrl + D
+
+# >>> 1+2
+# 3
+# >>> 3/2.4
+# 1.25
+# >>> 3*9
+# 27
+# >>> a=1
+# >>> b=2
+# >>> a+b
+# 3
+# >>> a="Python"
+# >>> print(a)
+# Python
+# complex
+# >>> a = 2+3j
+# >>> b = 3
+# >>> a*b
+# (6+9j)
+
+# if
+# >>> a = 3
+# >>> if a>1:
+# ...     print("a is greater than 1")
+# ...
+# a is greater than 1
+
+# for
+# >>> for a in [1,2,3]:
+# ...     print(a)
+# ...
+# 1
+# 2
+# 3
+
+
