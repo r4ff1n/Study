f5e83b67d732c4d0df666312e21c49860dd45463
r4ff1n <r4ff1n@proton.me>
2025-01-19 23:02:59 +0900

    2025.01.19

--- Python/codeup/6068.py
+++ Python/codeup/6068.py
@@ -1,10 +1,14 @@
-a=input()
-if a=='A':
-    print('best!!!')
-elif a=='B':
-    print('good!!')
-elif a=='C':
-    print('run!')
-elif a=='D':
-    print('slowly~')  
-else:print("what?")
+a=int(input())
+if a>=90 :
+  print('A')
+else :
+  if a>=70 :
+    print('B')
+  else:
+    if a>=40 :
+      print('C')
+    else:
+      print('D')  
+
+# 한 줄 코딩
+a=int(input());print(("A" if a<0 else "C") if a%2==0 else ("B" if a<0 else "D"))
---
+++ Python/codeup/6071.py
@@ -0,0 +1,10 @@
+a=1
+while(a!=0):
+    a=int(input())
+    if a!=0: 
+        print(a)
+    
+    
+
+# 다른 풀이
+# while(a:=int(input()))!=0:print(a)
