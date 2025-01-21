8cd48f7b6034bac0dc9c5c1fa66bb477ac069c5c
r4ff1n <r4ff1n@proton.me>
2025-01-21 21:04:59 +0900

    2025.01.21(Python)

--- Python/codeup/6098.py
+++ Python/codeup/6098.py
@@ -1,23 +1,25 @@
-a = [list(map(int, input().split())) for _ in range(10)]
-
+a=[list(map(int, input().split())) for _ in range(10)]
+a[1][1]=9
 for i in range(1,9):
-    for j in range(1,9):
-        if a[i][j]==9:
-            if a[i][j+1] != 1:
-                if a[i][j+1] == 2:
-                    a[i][j+1] = 9
-                    break
-                a[i][j+1] = 9
-            elif a[i+1][j] != 1:
-                if a[i+1][j] == 2:
-                    a[i+1][j] = 9
-                    break
-                a[i+1][j] = 9
-            else:
-                break
-    else:
-        continue
-    break
+  for j in range(1,9):
+    if a[i][j]==9:
+      if a[i][j+1]!=1:
+        if a[i][j+1]==2:
+          a[i][j+1]=9
+          break
+        a[i][j+1]=9
+      elif a[i+1][j] != 1:
+          if a[i+1][j] == 2:
+              a[i+1][j] = 9
+              break
+          a[i+1][j] = 9
+      else:
+        break
+  else:
+    continue
+  break
+for _ in a:
+    print(*_)
 
 # 완성된 풀이
 maze = [list(map(int, input().split())) for _ in range(10)]
