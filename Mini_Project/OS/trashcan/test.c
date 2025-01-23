2d262035b8f750fd92994c8c5d1556fdda228e3e
r4ff1n <r4ff1n@proton.me>
2025-01-23 18:53:27 +0900

    2025.01.23(Mini_project eject)

--- Mini_Project/OS/trashcan/test.c
+++
@@ -1,9 +0,0 @@
-// gcc -m32 -o test32 test.c
-// gcc -m64 -o test64 test.c
-// if you use "gcc-14", apt install gcc-14-multilib
-#include <stdio.h>
-
-int main(int argc, char** argv){
-    printf("Hello, world\n");
-    return 0;
-}
