50e7c6b4e26ea6ba3efc879754914fd42b55c88b
r4ff1n <r4ff1n@proton.me>
2025-01-23 14:57:55 +0900

    2025.01.23

---
+++ Mini_Project/OS/trashcan/test.c
@@ -0,0 +1,8 @@
+// gcc -m32 -o test32 test.c
+// gcc -m64 -o test64 test.c
+#include <stdio.h>
+
+int main(int argc, char** argv){
+    printf("Hello, world\n");
+    return 0;
+}
---
+++ Mini_Project/Python/001.py
@@ -0,0 +1,6 @@
+import os
+import platform
+system=platform.system()
+# print(system)
+# if system=="Windows":os.system("shudown /s /t 0")
+# elif system == "Linux" or system == "Darwin":os.system("init 0")
