6bd88236a69e2d754ddaed8055b0c0a98bbada85
r4ff1n <r4ff1n@proton.me>
2025-02-04 17:30:07 +0900

    2025.02.04

--- C_Lang/Certification/test.c
+++ C_Lang/Certification/test.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 int main(){
     int a[3] = {1,2};
+    int *p=a;
     printf("%d %d %d\n",a[0],a[1],a[2]);
-    printf("%d %d %d\n",*a,*a(a+1),a(a+2));
-    printf("%d %d %d\n",a[0],a[1],a[2]);
-    printf("%d %d %d\n",a[0],a[1],a[2]);
+    printf("%d %d %d\n",*a,*(a+1),*(a+2));
+    printf("%d %d %d\n",*p,*(p+1),*(p+2));
+    printf("%d %d %d\n",p[0],p[1],p[2]);
 
 }
